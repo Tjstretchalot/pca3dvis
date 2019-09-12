c979977624f26016118872e3ff66c7d9ff07be8d
Timothy Moore <mtimothy984@gmail.com>
2019-09-12 09:49:24 -0700

    Add more comments to gaussian balls example

--- examples/gaus_balls.py
+++ examples/gaus_balls.py
@@ -7,7 +7,7 @@ import numpy as np
 
 FEATURES = 10
 """The embedded space of the generated data. Every later snapshot
-has one more feature just to verify that doesn't hurt anything"""
+has one more feature just to see that doesn't hurt anything"""
 CLUSTERS = 5
 """How many clusters are made in the embedding space"""
 SNAPSHOTS = 2
@@ -17,7 +17,7 @@ SAMPLES_PER_CLUST = 200
 CLUST_STD = 0.2
 """Standard deviation of each cluster"""
 DRAFT = True
-"""If draft settings are used"""
+"""If draft settings are used for the video (ie. lower quality, but faster)"""
 
 def gaus_ball(center: np.ndarray, std: int, num_samples: int):
     """Produces a gaussian ball with the given center and std deviation"""
@@ -27,10 +27,17 @@ def gaus_ball(center: np.ndarray, std: int, num_samples: int):
     )
 
 def _main():
-
+    # First generate some data!
     datas = []
     for snap in range(SNAPSHOTS):
+        # We are looping over each of the snapshots we have. Since we are
+        # generating points randomly, these snapshots are meaningless, but
+        # we will see how these points are rendered
+
         centers = np.random.uniform(-2, 2, (CLUSTERS, FEATURES + snap))
+        # Get a center for each cluster uniformly on a cube with sidelengths
+        # 4 centered at the origin
+
         data = np.concatenate(
             tuple(
                 gaus_ball(cent, CLUST_STD, SAMPLES_PER_CLUST)
@@ -38,6 +45,9 @@ def _main():
             ),
             0
         )
+        # Sample 200 points from each cluster and append them to the data
+        # (but faster)
+
         datas.append(data)
 
     lbls = np.concatenate(
@@ -47,27 +57,34 @@ def _main():
         ),
         0
     )
+    # Each cluster has its own label
 
     cmap = plt.get_cmap('Set1')
-    markers = [
-        (
-            np.ones(lbls.shape, dtype='bool'),
+    markers = [ # An array with 1 marker for each point
+        ( # The first marker
+            np.ones(lbls.shape, dtype='bool'), # a mask containing every point
             {
-                'c': lbls,
-                'cmap': cmap,
-                's': 20,
-                'marker': 'o',
-                'norm': mcolors.Normalize(0, CLUSTERS - 1)
+                'c': lbls, # color these points based on their label
+                'cmap': cmap, # to decide to color from the label, use the colormap
+                's': 20, # the points should be around 20px
+                'marker': 'o', # use a circle to represent these points
+                'norm': mcolors.Normalize(0, CLUSTERS - 1) # the smallest label is 0, largest is CLUSTERS-1
             }
         )
     ]
 
     proj = pcs.get_pc_trajectory(datas, lbls)
+    # This performs linear dimensionality-reduction using principal component
+    # analysis to get the three-dimensional points we can actually plot
 
     worker.generate(
-        proj, markers, ['Gaussian Balls (1)', 'Gaussian Balls (2)'],
-        'out/examples/gaus_balls', DRAFT
+        proj,  # Plot the points we found
+        markers,  # use the markers we made earlier
+        ['Gaussian Balls (1)', 'Gaussian Balls (2)'], # title the different slices as so
+        'out/examples/gaus_balls',  # store the result in this folder
+        DRAFT # determines if we should store low quality (if True) or high quality (if False)
     )
+    # That's it!
 
 if __name__ == '__main__':
     _main()
